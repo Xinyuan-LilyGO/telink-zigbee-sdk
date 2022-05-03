7ca3b9b9b28b23f1a6925fba9c58bf7866d4be5d
liangyingy <1102390310@qq.com>
2022-05-03 17:52:34 +0800

    feature: add fullDevice project
    Signed-off-by: liangyingy <1102390310@qq.com>

---
+++ tl_zigbee_sdk/apps/fullDevice/app_ui.c
@@ -0,0 +1,275 @@
+/********************************************************************************************************
+ * @file	app_ui.c
+ *
+ * @brief	This is the source file for app_ui
+ *
+ * @author	Zigbee Group
+ * @date	2019
+ *
+ * @par     Copyright (c) 2019, Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
+ *          All rights reserved.
+ *
+ *          Redistribution and use in source and binary forms, with or without
+ *          modification, are permitted provided that the following conditions are met:
+ *
+ *              1. Redistributions of source code must retain the above copyright
+ *              notice, this list of conditions and the following disclaimer.
+ *
+ *              2. Unless for usage inside a TELINK integrated circuit, redistributions
+ *              in binary form must reproduce the above copyright notice, this list of
+ *              conditions and the following disclaimer in the documentation and/or other
+ *              materials provided with the distribution.
+ *
+ *              3. Neither the name of TELINK, nor the names of its contributors may be
+ *              used to endorse or promote products derived from this software without
+ *              specific prior written permission.
+ *
+ *              4. This software, with or without modification, must only be used with a
+ *              TELINK integrated circuit. All other usages are subject to written permission
+ *              from TELINK and different commercial license may apply.
+ *
+ *              5. Licensee shall be solely responsible for any claim to the extent arising out of or
+ *              relating to such deletion(s), modification(s) or alteration(s).
+ *
+ *          THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *          ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *          WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *          DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+ *          DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *          (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *          LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *          ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *          (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *          SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************************************/
+#if (__PROJECT_TL_GW__)
+
+/**********************************************************************
+ * INCLUDES
+ */
+#include "tl_common.h"
+#include "zb_api.h"
+#include "zcl_include.h"
+#include "sampleGateway.h"
+#include "app_ui.h"
+
+
+/**********************************************************************
+ * LOCAL CONSTANTS
+ */
+
+
+/**********************************************************************
+ * TYPEDEFS
+ */
+
+
+/**********************************************************************
+ * LOCAL FUNCTIONS
+ */
+void led_on(u32 pin)
+{
+	drv_gpio_write(pin, LED_ON);
+}
+
+void led_off(u32 pin)
+{
+	drv_gpio_write(pin, LED_OFF);
+}
+
+void light_on(void)
+{
+	led_on(LED_POWER);
+}
+
+void light_off(void)
+{
+	led_off(LED_POWER);
+}
+
+void light_init(void)
+{
+	led_on(LED_POWER);
+}
+
+s32 zclLightTimerCb(void *arg)
+{
+	u32 interval = 0;
+
+	if(g_appGwCtx.sta == g_appGwCtx.oriSta){
+		g_appGwCtx.times--;
+		if(g_appGwCtx.times <= 0){
+			g_appGwCtx.timerLedEvt = NULL;
+			return -1;
+		}
+	}
+
+	g_appGwCtx.sta = !g_appGwCtx.sta;
+	if(g_appGwCtx.sta){
+		light_on();
+		interval = g_appGwCtx.ledOnTime;
+	}else{
+		light_off();
+		interval = g_appGwCtx.ledOffTime;
+	}
+
+	return interval;
+}
+
+void light_blink_start(u8 times, u16 ledOnTime, u16 ledOffTime)
+{
+	u32 interval = 0;
+	g_appGwCtx.times = times;
+
+	if(!g_appGwCtx.timerLedEvt){
+		if(g_appGwCtx.oriSta){
+			light_off();
+			g_appGwCtx.sta = 0;
+			interval = ledOffTime;
+		}else{
+			light_on();
+			g_appGwCtx.sta = 1;
+			interval = ledOnTime;
+		}
+		g_appGwCtx.ledOnTime = ledOnTime;
+		g_appGwCtx.ledOffTime = ledOffTime;
+
+		g_appGwCtx.timerLedEvt = TL_ZB_TIMER_SCHEDULE(zclLightTimerCb, NULL, interval);
+	}
+}
+
+void light_blink_stop(void)
+{
+	if(g_appGwCtx.timerLedEvt){
+		TL_ZB_TIMER_CANCEL(&g_appGwCtx.timerLedEvt);
+
+		g_appGwCtx.times = 0;
+		if(g_appGwCtx.oriSta){
+			light_on();
+		}else{
+			light_off();
+		}
+	}
+}
+
+
+void buttonKeepPressed(u8 btNum){
+	if(btNum == VK_SW1){
+		//zb_factoryReset();
+	}else if(btNum == VK_SW2){
+
+	}
+}
+
+ev_timer_event_t *brc_toggleEvt = NULL;
+s32 brc_toggleCb(void *arg)
+{
+	static bool toggle = 0;
+
+	epInfo_t dstEpInfo;
+	TL_SETSTRUCTCONTENT(dstEpInfo, 0);
+
+	dstEpInfo.dstAddrMode = APS_SHORT_DSTADDR_WITHEP;
+	dstEpInfo.dstEp = SAMPLE_GW_ENDPOINT;
+	dstEpInfo.dstAddr.shortAddr = 0xffff;
+	dstEpInfo.profileId = HA_PROFILE_ID;
+	dstEpInfo.txOptions = 0;
+	dstEpInfo.radius = 0;
+
+	//zcl_onOff_toggleCmd(SAMPLE_GW_ENDPOINT, &dstEpInfo, FALSE);
+	toggle = ~toggle;
+	if(toggle){
+		zcl_onOff_onCmd(SAMPLE_GW_ENDPOINT, &dstEpInfo, FALSE);
+	}else{
+		zcl_onOff_offCmd(SAMPLE_GW_ENDPOINT, &dstEpInfo, FALSE);
+	}
+
+	return 0;
+}
+
+void brc_toggle()
+{
+	if(!brc_toggleEvt){
+		brc_toggleEvt = TL_ZB_TIMER_SCHEDULE(brc_toggleCb, NULL, 1000);
+	}else{
+		TL_ZB_TIMER_CANCEL(&brc_toggleEvt);
+	}
+}
+
+void buttonShortPressed(u8 btNum){
+	if(btNum == VK_SW1){
+		if(zb_isDeviceJoinedNwk()){
+#if POLL_CTRL_SUPPORT
+			sampleGW_zclFastPollStopCmdSend();
+#else
+#if 0
+			epInfo_t dstEpInfo;
+			TL_SETSTRUCTCONTENT(dstEpInfo, 0);
+
+			dstEpInfo.dstAddrMode = APS_SHORT_DSTADDR_WITHEP;
+			dstEpInfo.dstEp = SAMPLE_GW_ENDPOINT;
+			dstEpInfo.dstAddr.shortAddr = 0xffff;
+			dstEpInfo.profileId = HA_PROFILE_ID;
+			dstEpInfo.txOptions = 0;
+			dstEpInfo.radius = 0;
+
+			zcl_onOff_toggleCmd(SAMPLE_GW_ENDPOINT, &dstEpInfo, FALSE);
+#else
+			brc_toggle();
+#endif
+#endif
+		}
+	}else if(btNum == VK_SW2){
+		if(zb_isDeviceJoinedNwk()){
+			/* toggle local permit Joining */
+			static u8 duration = 0;
+			duration = duration ? 0 : 0xff;
+			zb_nlmePermitJoiningRequest(duration);
+		}
+	}
+}
+
+void keyScan_keyPressedCB(kb_data_t *kbEvt){
+//	u8 toNormal = 0;
+	u8 keyCode = kbEvt->keycode[0];
+//	static u8 lastKeyCode = 0xff;
+
+	buttonShortPressed(keyCode);
+
+	if(keyCode == VK_SW1){
+		g_appGwCtx.keyPressedTime = clock_time();
+		g_appGwCtx.state = APP_FACTORY_NEW_SET_CHECK;
+	}
+}
+
+
+void keyScan_keyReleasedCB(u8 keyCode){
+	g_appGwCtx.state = APP_STATE_NORMAL;
+}
+
+volatile u8 T_keyPressedNum = 0;
+void app_key_handler(void){
+	static u8 valid_keyCode = 0xff;
+
+	if(g_appGwCtx.state == APP_FACTORY_NEW_SET_CHECK){
+		if(clock_time_exceed(g_appGwCtx.keyPressedTime, 5*1000*1000)){
+			buttonKeepPressed(VK_SW1);
+		}
+	}
+
+	if(kb_scan_key(0 , 1)){
+		T_keyPressedNum++;
+		if(kb_event.cnt){
+			keyScan_keyPressedCB(&kb_event);
+			if(kb_event.cnt == 1){
+				valid_keyCode = kb_event.keycode[0];
+			}
+		}else{
+			keyScan_keyReleasedCB(valid_keyCode);
+			valid_keyCode = 0xff;
+		}
+	}
+}
+
+#endif  /* __PROJECT_TL_GW__ */
---
+++ tl_zigbee_sdk/apps/fullDevice/sampleGateway.h
@@ -0,0 +1,146 @@
+/********************************************************************************************************
+ * @file	sampleGateway.h
+ *
+ * @brief	This is the header file for sampleGateway
+ *
+ * @author	Zigbee Group
+ * @date	2019
+ *
+ * @par     Copyright (c) 2019, Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
+ *          All rights reserved.
+ *
+ *          Redistribution and use in source and binary forms, with or without
+ *          modification, are permitted provided that the following conditions are met:
+ *
+ *              1. Redistributions of source code must retain the above copyright
+ *              notice, this list of conditions and the following disclaimer.
+ *
+ *              2. Unless for usage inside a TELINK integrated circuit, redistributions
+ *              in binary form must reproduce the above copyright notice, this list of
+ *              conditions and the following disclaimer in the documentation and/or other
+ *              materials provided with the distribution.
+ *
+ *              3. Neither the name of TELINK, nor the names of its contributors may be
+ *              used to endorse or promote products derived from this software without
+ *              specific prior written permission.
+ *
+ *              4. This software, with or without modification, must only be used with a
+ *              TELINK integrated circuit. All other usages are subject to written permission
+ *              from TELINK and different commercial license may apply.
+ *
+ *              5. Licensee shall be solely responsible for any claim to the extent arising out of or
+ *              relating to such deletion(s), modification(s) or alteration(s).
+ *
+ *          THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *          ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *          WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *          DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+ *          DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *          (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *          LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *          ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *          (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *          SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************************************/
+#ifndef _SAMPLE_GW_H_
+#define _SAMPLE_GW_H_
+
+
+/**********************************************************************
+ * CONSTANT
+ */
+#define SAMPLE_GW_ENDPOINT      0x01
+#define SAMPLE_TEST_ENDPOINT	0x02
+
+/**********************************************************************
+ * TYPEDEFS
+ */
+typedef struct{
+	ev_timer_event_t *timerLedEvt;
+	u32 keyPressedTime;
+
+	u16 ledOnTime;
+	u16 ledOffTime;
+	u8 	oriSta;		//original state before blink
+	u8 	sta;		//current state in blink
+	u8 	times;		//blink times
+	u8  state;
+	u8	keyPressed;
+}app_ctx_t;
+
+/**
+ *  @brief Defined for basic cluster attributes
+ */
+typedef struct{
+	u8 	zclVersion;
+	u8	appVersion;
+	u8	stackVersion;
+	u8	hwVersion;
+	u8	manuName[ZCL_BASIC_MAX_LENGTH];
+	u8	modelId[ZCL_BASIC_MAX_LENGTH];
+	u8	powerSource;
+	u8	deviceEnable;
+}zcl_basicAttr_t;
+
+/**
+ *  @brief Defined for identify cluster attributes
+ */
+typedef struct{
+	u16	identifyTime;
+}zcl_identifyAttr_t;
+
+
+/**********************************************************************
+ * GLOBAL VARIABLES
+ */
+extern app_ctx_t g_appGwCtx;
+
+extern bdb_appCb_t g_zbDemoBdbCb;
+
+extern u8 SAMPLE_GW_CB_CLUSTER_NUM;
+extern const zcl_specClusterInfo_t g_sampleGwClusterList[];
+extern const af_simple_descriptor_t sampleGW_simpleDesc;
+#if AF_TEST_ENABLE
+extern const af_simple_descriptor_t sampleTestDesc;
+#endif
+
+/* Attributes */
+extern zcl_basicAttr_t g_zcl_basicAttrs;
+extern zcl_identifyAttr_t g_zcl_identifyAttrs;
+
+
+/**********************************************************************
+ * FUNCTIONS
+ */
+void sampleGW_zclProcessIncomingMsg(zclIncoming_t *pInHdlrMsg);
+
+status_t sampleGW_basicCb(zclIncomingAddrInfo_t *pAddrInfo, u8 cmdId, void *cmdPayload);
+status_t sampleGW_identifyCb(zclIncomingAddrInfo_t *pAddrInfo, u8 cmdId, void *cmdPayload);
+status_t sampleGW_groupCb(zclIncomingAddrInfo_t *pAddrInfo, u8 cmdId, void *cmdPayload);
+status_t sampleGW_sceneCb(zclIncomingAddrInfo_t *pAddrInfo, u8 cmdId, void *cmdPayload);
+#ifdef ZCL_DOOR_LOCK
+status_t sampleGW_doorLockCb(zclIncomingAddrInfo_t *pAddrInfo, u8 cmdId, void *cmdPayload);
+#endif
+#ifdef ZCL_IAS_ZONE
+status_t sampleGW_iasZoneCb(zclIncomingAddrInfo_t *pAddrInfo, u8 cmdId, void *cmdPayload);
+#endif
+#ifdef ZCL_POLL_CTRL
+status_t sampleGW_pollCtrlCb(zclIncomingAddrInfo_t *pAddrInfo, u8 cmdId, void *cmdPayload);
+#endif
+
+void sampleGW_devAnnHandler(zdo_device_annce_req_t *pDevAnnceReq);
+void sampleGW_leaveCnfHandler(nlme_leave_cnf_t *pLeaveCnf);
+void sampleGW_leaveIndHandler(nlme_leave_ind_t *pLeaveInd);
+bool sampleGW_nwkUpdateIndicateHandler(nwkCmd_nwkUpdate_t *pNwkUpdateCmd);
+bool sampleGW_tcJoinIndHandler(zdo_tc_join_ind_t *pTcJoinInd);
+void sampleGW_tcFrameCntReachedHandler(void);
+
+void sampleGW_dataSendConfirm(void *arg);
+
+#if AF_TEST_ENABLE
+void afTest_rx_handler(void *arg);
+void afTest_dataSendConfirm(void *arg);
+#endif
+
+#endif /* _SAMPLE_GW_H_ */
